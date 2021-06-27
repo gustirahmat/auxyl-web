52a257ab6991fbd1864360eb2d985733d8922963
Gusti Rahmat <gustirahmat@hotmail.com>
2021-06-27 14:27:14 +0700

    Commenting all api routes
    Signed-off-by: Gusti Rahmat <gustirahmat@hotmail.com>

--- Modules/Report/Routes/api.php
+++ Modules/Report/Routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/report', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/report', function (Request $request) {
+//    return $request->user();
+//});
