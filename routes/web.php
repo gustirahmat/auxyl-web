ceb1cf975ef6e7576cfe2f047210326b4e4a5e57
Gusti Rahmat <gustirahmat@hotmail.com>
2021-06-27 14:33:45 +0700

    Permanent redirect / to /home
    Signed-off-by: Gusti Rahmat <gustirahmat@hotmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,9 +14,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::permanentRedirect('/', '/home');
 
 Auth::routes();
 
