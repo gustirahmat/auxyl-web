04f84997a5cb5e9207f63f3302d80fabffe35524
Gusti Rahmat <gustirahmat@hotmail.com>
2021-07-03 13:37:33 +0700

    Commenting api route on Complain module
    Signed-off-by: Gusti Rahmat <gustirahmat@hotmail.com>

--- Modules/Complain/Routes/api.php
+++ Modules/Complain/Routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/complain', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/complain', function (Request $request) {
+//    return $request->user();
+//});
