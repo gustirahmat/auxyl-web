0d28e92a0661d7dc3cd41cc2b1ef9cdb5f5f45d6
finkastwn <winafinkastwn@gmail.com>
2021-08-11 20:54:58 +0700

    ubah nama promo menjadi promosi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('promo') }}">
                             <i class="bi bi-patch-minus"></i>
-                            Promo
+                            Promosi
                         </a>
                     </li>
                     <li class="nav-item">
